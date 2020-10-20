323a0ef8b6494b3654366348433faa376efd5446
adams-okode <adamsokode@gmail.com>
2020-10-20 12:16:53 +0300

    add ids

--- src/main/java/com/kseensei/springsecurityoauth20/entity/User.java
+++ src/main/java/com/kseensei/springsecurityoauth20/entity/User.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,7 +25,12 @@ public class User implements UserDetails {
      *
      */
     private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column(unique = true)
     private String username;
 
     private String password;
